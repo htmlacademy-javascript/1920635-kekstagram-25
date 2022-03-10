93bab3904143e7c2b1846fa941fe3bd48cccd36b
Андрей Чвартацкий <chvartatskii>
2022-03-10 20:32:37 +0300

    Добавил две функции

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function getRandom(min, max) {
+  if (min >= 0 && max > min){
+    return Math.random() * (max-min)+min;
+  }
+}
+getRandom(0, 10);
+
+
+function checkString (string, long){
+  if (string.length > long){
+    return false;
+  }
+  return true;
+}
+checkString('ss', 140);
