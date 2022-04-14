aff9da61e83072cfbf774ca36261a6d4bd97fc97
Андрей Чвартацкий <chvartatskii>
2022-04-14 22:11:38 +0300

    A

--- js/big-picture.js
+++ js/big-picture.js
@@ -4,14 +4,12 @@ const bigPicture = document.querySelector('.big-picture');
 const commentsList = bigPicture.querySelector('.social__comments');
 const bigPictureUrl = bigPicture.querySelector('.big-picture__img');
 const bigPictureLikesCounter = bigPicture.querySelector('.likes-count');
+const bigPictureCommentsCount = bigPicture.querySelector('.comments-count');
 const bigPictureDiscription = bigPicture.querySelector('.social__caption');
 const commentItem = commentsList.querySelector('.social__comment');
-const closeButton = bigPicture.querySelector('.big-picture__cancel');
 const commentsLoader = document.querySelector('.comments-loader');
-const commentsCounter = document.querySelector('.current-comment');
-let count = 0;
-let handleCommentsLoader;
-
+const closeButton = bigPicture.querySelector('.big-picture__cancel');
+let startComments = 0;
 
 const createComment = (comment) => {
   const commentElement = commentItem.cloneNode(true);
@@ -22,36 +20,27 @@ const createComment = (comment) => {
   return commentElement;
 };
 
-function downloadComment(comments) {
-  let finishCount = count + 5;
-  if (finishCount > comments.length) {
-    finishCount = comments.length;
+const download = (comments) => {
+  let endComments = startComments + 5 ;
+  if(endComments >= comments.length){
+    endComments = comments.length;
+    commentsLoader.remove();
   }
-  for (let i = count; i < finishCount; i++) {
+  for (let i = startComments; i < endComments; i++) {
     commentsList.appendChild(createComment(comments[i]));
   }
-  count += 5;
-  commentsCounter.textContent = finishCount;
-  if (count >= comments.length) {
-    commentsLoader.classList.add('hidden');
-    commentsLoader.removeEventListener('click', handleCommentsLoader);
-    count = 0;
-  }
-}
+  startComments += 5;
+};
 
 const showBigPicture = ({ url, likes, comments, description }) => {
+  commentsLoader.addEventListener('click', () => download(comments));
   bigPictureUrl.src = url;
   bigPictureLikesCounter.textContent = likes;
   bigPictureCommentsCount.textContent = comments.length;
   bigPictureDiscription.textContent = description;
   commentsList.textContent = '';
-  downloadComment(comments);
-  commentsLoader.classList.remove('hidden');
-  handleCommentsLoader = () => {
-    downloadComment(comments);
-  };
-  commentsLoader.addEventListener('click', handleCommentsLoader);
+  commentsList.innerHTML = '';
   openModal(bigPicture, closeButton);
+  download(comments);
 };
-
 export { showBigPicture };
