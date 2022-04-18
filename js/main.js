3bb5124a373ea24b37641f7f51f856b94c91abec
Андрей Чвартацкий <chvartatskii>
2022-04-18 22:58:08 +0300

    12 часть 1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 import {closeModal} from './open-modal.js';
 import {setUserFormSubmit} from './upload-form.js';
-import {renderSimilarPicture} from './create-picture.js';
+import {commentsCountPost, defaultPost, randomPost, renderSimilarPicture} from './create-picture.js';
 import {getData} from './api.js';
+const randomPostCount = 10;
+
 
 getData((post) => {
   renderSimilarPicture(post);
+  randomPost(post, randomPostCount);
+  defaultPost(post);
+  commentsCountPost(post);
 });
 
 setUserFormSubmit(closeModal);
